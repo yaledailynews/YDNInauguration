e8207657ffe4298e626cd8f5b6469a3566cc6f4b
YDNadmin <YDNadmin@client-24.ydn.yale.edu>
2013-10-11 07:03:49 -0400

    Improved layout

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+function synch() {
+
+    setHeight($('.twitter-timeline'), $('#tumblrfeed'));
+
+}
+
+setInterval(synch, 1000);
+
+// sets height of element 1 to equal the height of element 2
+function setHeight(elem1, elem2) {
+    var height = elem2.height()
+    elem1.css('height', height);
+}
+
+
+var tlog = "";
+
+function lookforchanges() {
+    $.get("http://xcampus.tumblr.com/js", checkdiff);
+}
+
+function checkdiff(data) {
+    if (data) {
+        if (data.responseText != tlog) {
+            tlog = data.responseText;
+            document.getElementById("leftframe").contentDocument.location.reload(true);
+            // $( ".tumblframe" ).remove();
+            // $("body").append("<iframe src='http://ydntech.github.io/YDNInauguration/jslook.html' class='tumblframe'><\/iframe>");
+        }
+    }
+}
+
+setInterval(lookforchanges, 30000);
+
+
+
+
+! function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+        p = /^http:/.test(d.location) ? 'http' : 'https';
+    if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + "://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }
+}(document, "script", "twitter-wjs");
